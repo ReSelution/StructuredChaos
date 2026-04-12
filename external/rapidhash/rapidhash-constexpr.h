c8cd9cf5d68a821c22eb5ffd0154105c572287ac
oleub <ole.ubben@gmail.com>
2026-04-12 19:01:12 +0200

    Added Rapid ConstExpr

---
+++ external/rapidhash/rapidhash-constexpr.h
@@ -0,0 +1,351 @@
+/*
+ * rapidhash V3 - Very fast, high quality, platform-independent hashing algorithm.
+ *
+ * Based on 'wyhash', by Wang Yi <godspeed_china@yeah.net>
+ * 
+ * Copyright (C) 2025 Nicolas De Carli
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * You can contact the author at:
+ *   - rapidhash source repository: https://github.com/Nicoshev/rapidhash
+ */
+
+#pragma once
+
+/*
+ *  Includes.
+ */
+#include <string.h>
+#include <type_traits>
+#include <cstddef>
+#include <cstdint>
+#include <iterator>  // for std::data, std::size
+#include <type_traits>
+#include <utility>
+#include "rapidhash.h"
+
+
+namespace rapid::constExpr {
+/*
+ *  Default secret parameters.
+ */
+    RAPIDHASH_CONSTEXPR uint64_t rapid_secret[8] = {
+            0x2d358dccaa6c78a5ull,
+            0x8bb84b93962eacc9ull,
+            0x4b33a62ed433d4a3ull,
+            0x4d5a2da51de1aa47ull,
+            0xa0761d6478bd642full,
+            0xe7037ed1a0b428dbull,
+            0x90ed1765281c388cull,
+            0xaaaaaaaaaaaaaaaaull};
+
+// Credit: https://github.com/chys87/constexpr-xxh3/blob/main/constexpr-xxh3.h#L49
+    template<typename T>
+    concept ByteType = (std::is_integral_v<T> && sizeof(T) == 1)
+                       #if defined __cpp_lib_byte && __cpp_lib_byte >= 201603
+                       || std::is_same_v<T, std::byte>
+#endif
+    ;
+
+    template<typename T>
+    concept BytePtrType = requires(T ptr) {
+        requires std::is_pointer_v<T>;
+        requires ByteType<std::remove_cvref_t<decltype(*ptr)>>;
+    };
+
+    template<typename T>
+    concept BytesType = requires(const T &bytes) {
+        { std::data(bytes) };
+        requires BytePtrType<decltype(std::data(bytes))>;
+        // -> std::convertible_to is not supported widely enough
+        { static_cast<size_t>(std::size(bytes)) };
+    };
+
+/*
+ *  64*64 -> 128bit multiply function.
+ *
+ *  @param A  Address of 64-bit number.
+ *  @param B  Address of 64-bit number.
+ *
+ *  Calculates 128-bit C = *A * *B.
+ *
+ *  When RAPIDHASH_FAST is defined:
+ *  Overwrites A contents with C's low 64 bits.
+ *  Overwrites B contents with C's high 64 bits.
+ *
+ *  When RAPIDHASH_PROTECTED is defined:
+ *  Xors and overwrites A contents with C's low 64 bits.
+ *  Xors and overwrites B contents with C's high 64 bits.
+ */
+    RAPIDHASH_INLINE_CONSTEXPR void rapid_mum(uint64_t *A, uint64_t *B) RAPIDHASH_NOEXCEPT {
+#if defined(__SIZEOF_INT128__)
+        __uint128_t r = *A;
+        r *= *B;
+#ifdef RAPIDHASH_PROTECTED
+        *A^=(uint64_t)r; *B^=(uint64_t)(r>>64);
+#else
+        *A = (uint64_t) r;
+        *B = (uint64_t) (r >> 64);
+#endif
+#elif defined(_MSC_VER) && (defined(_WIN64) || defined(_M_HYBRID_CHPE_ARM64))
+#if defined(_M_X64)
+#ifdef RAPIDHASH_PROTECTED
+        uint64_t a, b;
+        a=_umul128(*A,*B,&b);
+        *A^=a;  *B^=b;
+#else
+        *A=_umul128(*A,*B,B);
+#endif
+#else
+#ifdef RAPIDHASH_PROTECTED
+        uint64_t a, b;
+        b = __umulh(*A, *B);
+        a = *A * *B;
+        *A^=a;  *B^=b;
+#else
+        uint64_t c = __umulh(*A, *B);
+        *A = *A * *B;
+        *B = c;
+#endif
+#endif
+#else
+      uint64_t ha=*A>>32, hb=*B>>32, la=(uint32_t)*A, lb=(uint32_t)*B;
+      uint64_t rh=ha*hb, rm0=ha*lb, rm1=hb*la, rl=la*lb, t=rl+(rm0<<32), c=t<rl;
+      uint64_t lo=t+(rm1<<32);
+      c+=lo<t;
+      uint64_t hi=rh+(rm0>>32)+(rm1>>32)+c;
+#ifdef RAPIDHASH_PROTECTED
+      *A^=lo;  *B^=hi;
+#else
+      *A=lo;  *B=hi;
+#endif
+#endif
+    }
+
+/*
+ *  Multiply and xor mix function.
+ *
+ *  @param A  64-bit number.
+ *  @param B  64-bit number.
+ *
+ *  Calculates 128-bit C = A * B.
+ *  Returns 64-bit xor between high and low 64 bits of C.
+ */
+    RAPIDHASH_INLINE_CONSTEXPR uint64_t rapid_mix(uint64_t A, uint64_t B) RAPIDHASH_NOEXCEPT {
+        rapid_mum(&A, &B);
+        return A ^ B;
+    }
+
+/*
+ *  Read functions.
+ */
+#ifdef RAPIDHASH_LITTLE_ENDIAN
+
+    template<ByteType T>
+    RAPIDHASH_INLINE_CONSTEXPR uint64_t rapid_read64(const T *p) RAPIDHASH_NOEXCEPT {
+        return (static_cast<uint64_t>(static_cast<uint8_t>(p[0])) << 0) |
+               (static_cast<uint64_t>(static_cast<uint8_t>(p[1])) << 8) |
+               (static_cast<uint64_t>(static_cast<uint8_t>(p[2])) << 16) |
+               (static_cast<uint64_t>(static_cast<uint8_t>(p[3])) << 24) |
+               (static_cast<uint64_t>(static_cast<uint8_t>(p[4])) << 32) |
+               (static_cast<uint64_t>(static_cast<uint8_t>(p[5])) << 40) |
+               (static_cast<uint64_t>(static_cast<uint8_t>(p[6])) << 48) |
+               (static_cast<uint64_t>(static_cast<uint8_t>(p[7])) << 56);
+
+    }
+
+    template<ByteType T>
+    RAPIDHASH_INLINE_CONSTEXPR uint64_t rapid_read32(const T *p) RAPIDHASH_NOEXCEPT {
+        return (static_cast<uint32_t>(static_cast<uint8_t>(p[0])) << 0) |
+               (static_cast<uint32_t>(static_cast<uint8_t>(p[1])) << 8) |
+               (static_cast<uint32_t>(static_cast<uint8_t>(p[2])) << 16) |
+               (static_cast<uint32_t>(static_cast<uint8_t>(p[3])) << 24);
+    }
+
+#elif defined(__GNUC__) || defined(__INTEL_COMPILER) || defined(__clang__)
+    RAPIDHASH_INLINE uint64_t rapid_read64(const uint8_t *p) RAPIDHASH_NOEXCEPT { uint64_t v; memcpy(&v, p, sizeof(uint64_t)); return __builtin_bswap64(v);}
+    RAPIDHASH_INLINE uint64_t rapid_read32(const uint8_t *p) RAPIDHASH_NOEXCEPT { uint32_t v; memcpy(&v, p, sizeof(uint32_t)); return __builtin_bswap32(v);}
+#elif defined(_MSC_VER)
+    RAPIDHASH_INLINE uint64_t rapid_read64(const uint8_t *p) RAPIDHASH_NOEXCEPT { uint64_t v; memcpy(&v, p, sizeof(uint64_t)); return _byteswap_uint64(v);}
+    RAPIDHASH_INLINE uint64_t rapid_read32(const uint8_t *p) RAPIDHASH_NOEXCEPT { uint32_t v; memcpy(&v, p, sizeof(uint32_t)); return _byteswap_ulong(v);}
+#else
+    RAPIDHASH_INLINE uint64_t rapid_read64(const uint8_t *p) RAPIDHASH_NOEXCEPT {
+      uint64_t v; memcpy(&v, p, 8);
+      return (((v >> 56) & 0xff)| ((v >> 40) & 0xff00)| ((v >> 24) & 0xff0000)| ((v >>  8) & 0xff000000)| ((v <<  8) & 0xff00000000)| ((v << 24) & 0xff0000000000)| ((v << 40) & 0xff000000000000)| ((v << 56) & 0xff00000000000000));
+    }
+    RAPIDHASH_INLINE uint64_t rapid_read32(const uint8_t *p) RAPIDHASH_NOEXCEPT {
+      uint32_t v; memcpy(&v, p, 4);
+      return (((v >> 24) & 0xff)| ((v >>  8) & 0xff00)| ((v <<  8) & 0xff0000)| ((v << 24) & 0xff000000));
+    }
+#endif
+
+/*
+ *  rapidhash main function.
+ *
+ *  @param key     Buffer to be hashed.
+ *  @param len     @key length, in bytes.
+ *  @param seed    64-bit seed used to alter the hash result predictably.
+ *  @param secret  Triplet of 64-bit secrets used to alter hash result predictably.
+ *
+ *  Returns a 64-bit hash.
+ */
+
+    template<ByteType T>
+    RAPIDHASH_INLINE_CONSTEXPR uint64_t
+    rapidhash_internal_constexpr(const T *key, size_t len, uint64_t seed, const uint64_t *secret) RAPIDHASH_NOEXCEPT {
+        const T *p = key;
+        seed ^= rapid_mix(seed ^ secret[2], secret[1]);
+        uint64_t a = 0, b = 0;
+        size_t i = len;
+        if (_likely_(len <= 16)) {
+            if (len >= 4) {
+                seed ^= len;
+                if (len >= 8) {
+                    const T *plast = p + len - 8;
+                    a = rapid_read64(p);
+                    b = rapid_read64(plast);
+                } else {
+                    const T *plast = p + len - 4;
+                    a = rapid_read32(p);
+                    b = rapid_read32(plast);
+                }
+            } else if (len > 0) {
+                a = (((uint64_t) p[0]) << 45) | p[len - 1];
+                b = p[len >> 1];
+            } else
+                a = b = 0;
+        } else {
+            if (len > 112) {
+                uint64_t see1 = seed, see2 = seed;
+                uint64_t see3 = seed, see4 = seed;
+                uint64_t see5 = seed, see6 = seed;
+#ifdef RAPIDHASH_COMPACT
+                do {
+                    seed = rapid_mix(rapid_read64(p) ^ secret[0], rapid_read64(p + 8) ^ seed);
+                    see1 = rapid_mix(rapid_read64(p + 16) ^ secret[1], rapid_read64(p + 24) ^ see1);
+                    see2 = rapid_mix(rapid_read64(p + 32) ^ secret[2], rapid_read64(p + 40) ^ see2);
+                    see3 = rapid_mix(rapid_read64(p + 48) ^ secret[3], rapid_read64(p + 56) ^ see3);
+                    see4 = rapid_mix(rapid_read64(p + 64) ^ secret[4], rapid_read64(p + 72) ^ see4);
+                    see5 = rapid_mix(rapid_read64(p + 80) ^ secret[5], rapid_read64(p + 88) ^ see5);
+                    see6 = rapid_mix(rapid_read64(p + 96) ^ secret[6], rapid_read64(p + 104) ^ see6);
+                    p += 112;
+                    i -= 112;
+                } while (i > 112);
+#else
+                while (i > 224) {
+                  seed = rapid_mix(rapid_read64(p) ^ secret[0], rapid_read64(p + 8) ^ seed);
+                  see1 = rapid_mix(rapid_read64(p + 16) ^ secret[1], rapid_read64(p + 24) ^ see1);
+                  see2 = rapid_mix(rapid_read64(p + 32) ^ secret[2], rapid_read64(p + 40) ^ see2);
+                  see3 = rapid_mix(rapid_read64(p + 48) ^ secret[3], rapid_read64(p + 56) ^ see3);
+                  see4 = rapid_mix(rapid_read64(p + 64) ^ secret[4], rapid_read64(p + 72) ^ see4);
+                  see5 = rapid_mix(rapid_read64(p + 80) ^ secret[5], rapid_read64(p + 88) ^ see5);
+                  see6 = rapid_mix(rapid_read64(p + 96) ^ secret[6], rapid_read64(p + 104) ^ see6);
+                  seed = rapid_mix(rapid_read64(p + 112) ^ secret[0], rapid_read64(p + 120) ^ seed);
+                  see1 = rapid_mix(rapid_read64(p + 128) ^ secret[1], rapid_read64(p + 136) ^ see1);
+                  see2 = rapid_mix(rapid_read64(p + 144) ^ secret[2], rapid_read64(p + 152) ^ see2);
+                  see3 = rapid_mix(rapid_read64(p + 160) ^ secret[3], rapid_read64(p + 168) ^ see3);
+                  see4 = rapid_mix(rapid_read64(p + 176) ^ secret[4], rapid_read64(p + 184) ^ see4);
+                  see5 = rapid_mix(rapid_read64(p + 192) ^ secret[5], rapid_read64(p + 200) ^ see5);
+                  see6 = rapid_mix(rapid_read64(p + 208) ^ secret[6], rapid_read64(p + 216) ^ see6);
+                  p += 224;
+                  i -= 224;
+                }
+                if (i > 112) {
+                  seed = rapid_mix(rapid_read64(p) ^ secret[0], rapid_read64(p + 8) ^ seed);
+                  see1 = rapid_mix(rapid_read64(p + 16) ^ secret[1], rapid_read64(p + 24) ^ see1);
+                  see2 = rapid_mix(rapid_read64(p + 32) ^ secret[2], rapid_read64(p + 40) ^ see2);
+                  see3 = rapid_mix(rapid_read64(p + 48) ^ secret[3], rapid_read64(p + 56) ^ see3);
+                  see4 = rapid_mix(rapid_read64(p + 64) ^ secret[4], rapid_read64(p + 72) ^ see4);
+                  see5 = rapid_mix(rapid_read64(p + 80) ^ secret[5], rapid_read64(p + 88) ^ see5);
+                  see6 = rapid_mix(rapid_read64(p + 96) ^ secret[6], rapid_read64(p + 104) ^ see6);
+                  p += 112;
+                  i -= 112;
+                }
+#endif
+                seed ^= see1;
+                see2 ^= see3;
+                see4 ^= see5;
+                seed ^= see6;
+                see2 ^= see4;
+                seed ^= see2;
+            }
+            if (i > 16) {
+                seed = rapid_mix(rapid_read64(p) ^ secret[2], rapid_read64(p + 8) ^ seed);
+                if (i > 32) {
+                    seed = rapid_mix(rapid_read64(p + 16) ^ secret[2], rapid_read64(p + 24) ^ seed);
+                    if (i > 48) {
+                        seed = rapid_mix(rapid_read64(p + 32) ^ secret[1], rapid_read64(p + 40) ^ seed);
+                        if (i > 64) {
+                            seed = rapid_mix(rapid_read64(p + 48) ^ secret[1], rapid_read64(p + 56) ^ seed);
+                            if (i > 80) {
+                                seed = rapid_mix(rapid_read64(p + 64) ^ secret[2], rapid_read64(p + 72) ^ seed);
+                                if (i > 96) {
+                                    seed = rapid_mix(rapid_read64(p + 80) ^ secret[1], rapid_read64(p + 88) ^ seed);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            a = rapid_read64(p + i - 16) ^ i;
+            b = rapid_read64(p + i - 8);
+        }
+        a ^= secret[1];
+        b ^= seed;
+        rapid_mum(&a, &b);
+        return rapid_mix(a ^ secret[7], b ^ secret[1] ^ i);
+    }
+
+
+/*
+ *  rapidhash seeded hash function.
+ *
+ *  @param key     Buffer to be hashed.
+ *  @param len     @key length, in bytes.
+ *  @param seed    64-bit seed used to alter the hash result predictably.
+ *
+ *  Calls rapidhash_internal_constexpr using provided parameters and default secrets.
+ *
+ *  Returns a 64-bit hash.
+ */
+    template<ByteType T>
+    RAPIDHASH_INLINE_CONSTEXPR uint64_t
+    rapidhash_withSeed(const T *key, size_t len, uint64_t seed) RAPIDHASH_NOEXCEPT {
+        return rapidhash_internal_constexpr(key, len, seed, rapid_secret);
+    }
+
+/*
+ *  rapidhash general purpose hash function.
+ *
+ *  @param key     Buffer to be hashed.
+ *  @param len     @key length, in bytes.
+ *
+ *  Calls rapidhash_withSeed using provided parameters and the default seed.
+ *
+ *  Returns a 64-bit hash.
+ */
+    template<ByteType T>
+    RAPIDHASH_INLINE_CONSTEXPR uint64_t rapidhash(const T *key, size_t len) RAPIDHASH_NOEXCEPT {
+        return rapidhash_withSeed(key, len, 0);
+    }
+
+}
+
+
--- src/hash/hash.hpp
+++ src/hash/hash.hpp
@@ -8,7 +8,8 @@
 #include <tuple>
 #include <type_traits>
 #include <string_view>
-#include "rapidhash/rapidhash.h"
+#include "rapidhash.h"
+#include "rapidhash-constexpr.h"
 
 
 #if defined(_MSC_VER) // MSVC
@@ -18,13 +19,6 @@
 #else
 #define FORCE_INLINE inline
 #endif
-
-
-inline namespace literals {
-    consteval uint64_t operator ""_h(const char *s, size_t len) {
-        return rapidhash(s, len);
-    }
-}
 
 
 namespace SC {
@@ -44,30 +38,58 @@ namespace SC {
 
     template<typename ... Args>
     FORCE_INLINE constexpr h64 hash(Args &&...args) {
-        if constexpr (sizeof ... (Args) == 2) {
-            auto extract = [](auto &&first, auto &&second) {
-                return rapidhash(static_cast<const void *>(first), static_cast<size_t>(second));
-            };
-            return extract(std::forward<Args>(args)...);
-        } else if constexpr (sizeof ...(Args) == 1) {
+        if constexpr (sizeof...(Args) == 2) {
+            if consteval {
+                auto extract = [](auto &&first, auto &&second) constexpr {
+                    return rapid::constExpr::rapidhash(first, static_cast<size_t>(second));
+                };
+                return extract(std::forward<Args>(args)...);
+            } else {
+                auto extract = [](auto &&first, auto &&second) {
+                    return rapidhash(reinterpret_cast<const uint8_t *>(first), static_cast<size_t>(second));
+                };
+                return extract(std::forward<Args>(args)...);
+            }
+        } else if constexpr (sizeof...(Args) == 1) {
             auto &&arg = std::get<0>(std::forward_as_tuple(std::forward<Args>(args)...));
 
-            return rapidhash(arg.data(), arg.size());
+            auto dispatch = [](auto&& a) constexpr {
+                if constexpr (std::is_convertible_v<decltype(a), std::string_view>) {
+                    std::string_view sv = a;
+                    if consteval {
+                        return rapid::constExpr::rapidhash(sv.data(), sv.size());
+                    } else {
+                        return rapidhash(reinterpret_cast<const uint8_t*>(sv.data()), sv.size());
+                    }
+                } else {
+                    if consteval {
+                        return rapid::constExpr::rapidhash(a.data(), a.size());
+                    } else {
+                        return rapidhash(reinterpret_cast<const uint8_t*>(a.data()), a.size());
+                    }
+                }
+            };
+
+            return dispatch(arg);
         }
     }
 
     FORCE_INLINE constexpr h64 hash_lowercase(std::string_view str) {
         constexpr size_t MAX_STR_LEN = 512;
-        char buffer[MAX_STR_LEN];
+        uint8_t buffer[MAX_STR_LEN];
         const size_t len = std::min<size_t>(str.length(), MAX_STR_LEN);
 
         for (size_t i = 0; i < len; ++i) {
             const auto c = static_cast<uint8_t>(str[i]);
             buffer[i] = (c >= 'A' && c <= 'Z') ? static_cast<char>(c + 32) : static_cast<char>(c);
         }
-        return rapidhash(buffer, len);
+        if consteval {
+            return rapid::constExpr::rapidhash(buffer, len);
+        } else {
+            return rapidhash(buffer, len);
+        }
     }
-    
+
 
     template<typename T, typename ... Args>
     FORCE_INLINE constexpr T hash(Args &&...args) {
@@ -78,3 +100,8 @@ namespace SC {
 
 
 } // SC
+inline namespace literals {
+    constexpr SC::h64 operator ""_h(const char *s, size_t len) {
+        return SC::hash(s, len);
+    }
+}
