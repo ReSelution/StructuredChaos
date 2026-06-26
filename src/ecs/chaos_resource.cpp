4ae86d06cd565ebb3e44bf43d6497c97c49abdc7
oleub <ole.ubben@gmail.com>
2026-06-26 18:20:17 +0200

    cleanup

--- src/ecs/chaos_resource.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by oleub on 03.04.26.
-//
-
-#include "chaos_resource.hpp"
-
-namespace SC {
-} // SC
--- src/localizations/magpie_string.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by oleub on 18.04.26.
-//
-
-#include "magpie_string.hpp"
-
-namespace SC {
-} // SC
